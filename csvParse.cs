9d828234799b339e594b58a4afa113483532b0e1
Bill Keenan <bill@bigmojo.net>
2013-05-09 17:12:27 -0300

    Update csvParse.cs
    THis is pretty old, but was handy at the time.

--- csvParse.cs
+++ csvParse.cs
@@ -1,3 +1,13 @@
+/*
+So I have a csv file, and it has quote delimination, eg:
+one,two,"with , comma",four
+
+There are a million examples of baroque regex strings to parse these, but I couldn’t seem to get any of them to work. So I wrote a small bit of code to parse each line.
+
+Essentially pass each line into this code as a string, and it returns an array of values. Sorry for the bad spacing, wordpress y’all.
+*/
+
+
 class Program
 {
 public enum parseState
