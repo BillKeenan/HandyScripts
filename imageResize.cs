5884ad31e17f4382d06fa5d9aef9a3322aa13a55
Bill Keenan <bill@bigmojo.net>
2013-12-05 10:01:19 -0500

    added image resizer

---
+++ imageResize.cs
@@ -0,0 +1,23 @@
+public static Bitmap ActualResizeImage(Image image, int width, int height)
+        {
+            //the result
+            var result = new Bitmap(width, height);
+
+            // set the resolutions the same to avoid cropping due to resolution differences
+            result.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            //use a graphics object to draw the resized image into the bitmap
+            using (var graphics = Graphics.FromImage(result))
+            {
+                //set the resize quality modes to high quality
+                graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                //draw the image into the target bitmap
+                graphics.DrawImage(image, 0, 0, result.Width, result.Height);
+            }
+
+            //return the resulting bitmap
+            return result;
+        }
+      
