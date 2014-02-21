4ca96941024a7016117425f4d4e21b339f83fdb2
Bill Keenan <bill@bigmojo.net>
2014-02-21 13:32:28 -0500

    handling querystring on fb urls now

--- feedlyFacebookBookmarklet.js
+++ feedlyFacebookBookmarklet.js
@@ -1,20 +1,23 @@
-javascript:(function(){ var rssString = 'https://www.facebook.com/feeds/page.php?id=PAGEID&format=rss20';
-	var subscribeUrl = 'http://cloud.feedly.com/#subscription%2Ffeed%2F';
-	var docUrl = window.location.href;
-	var isId = docUrl.match(/\d+$/);
-	var id = 0;
-	if(isId){
-		id = isId
-	}else{
-		var graphUrl = docUrl.replace(/www/g,"graph");
-		var xmlhttp=new XMLHttpRequest();
-		xmlhttp.open("GET",graphUrl,false);
-		xmlhttp.send();
-		var json = JSON.parse(xmlhttp.responseText);
-		id = json.id;
-	}
-	var completeUrl = rssString.replace(/PAGEID/,id);
-	var encodedUri = encodeURI(completeUrl);
-	var finalUrl = subscribeUrl.concat(encodedUri);
-	window.location=finalUrl;
+<html>
+
+Drag this link onto your toolbar
+<a href="javascript:(function(){ 
+var rssString = 'https://www.facebook.com/feeds/page.php?id=PAGEID&format=rss20';
+var subscribeUrl = 'http://cloud.feedly.com/#subscription%2Ffeed%2F';
+var docUrl = window.location.href;
+var isId = docUrl.match(/(\d+)($|\?.*)/);
+var id = 0;
+if(isId){id = isId[1]}else{var graphUrl = docUrl.replace(/www/g,'graph');
+var xmlhttp=new XMLHttpRequest();
+xmlhttp.open('GET',graphUrl,false);
+xmlhttp.send();
+var json = JSON.parse(xmlhttp.responseText);
+id = json.id;
+}var completeUrl = rssString.replace(/PAGEID/,id);
+var encodedUri = encodeURI(completeUrl);
+var finalUrl = subscribeUrl.concat(encodedUri);
+window.location=finalUrl;
 })();
+">Facebook To Feedly</a>
+</html>
+
